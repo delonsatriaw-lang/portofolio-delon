2eff0b15d8695f39b36eadd58bd8e96d59e30ab3
Delon SW <delonsatriaw@gmail.com>
2025-11-24 13:00:37 +0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -33,14 +33,15 @@ export const SITE_CONTENT: SiteContent = {
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      organization: "Dewan Perwakilan Mahasiswa FTI",
+      position: "Public Relation Member",
+      startDate: "July 2024",
+      endDate: "July 2025",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Managed and maintained communication between the student body and the Faculty of Information Technology through social media platforms.",
+        "Created and edited digital design materials for organizational events and announcements.",
+        "Collaborated with other departments to coordinate faculty events and student engagement programs.",
+        "Assisted in developing creative content strategies to enhance the organization’s online presence.",
       ],
     },
     {
